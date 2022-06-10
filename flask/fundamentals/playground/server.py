8e14d2b25f77e23e30489d3ada89721bfb3f2516
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-06-09 17:34:51 -0700

    playground assignment

--- flask/fundamentals/playground/server.py
+++ flask/fundamentals/playground/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,10 +7,20 @@ def index():
     return "You're in the wrong page bro"
 
 @app.route("/play")
-    return render_templates("index.html")
+def start():    
+    return render_template("index.html", num = 3, color = "blue")
+
+@app.route("/play/<int:num>")
+def custom(num):
+    return render_template("index.html", num=num, color = "blue")
+
+@app.route("/play/<int:num>/<string:color>")
+def custom1(num, color):
+    return render_template("index.html", num=num, color = color)
 
 
 
+# http://127.0.0.1.5000
 
 if __name__=="__main__":
     app.run(debug=True)
