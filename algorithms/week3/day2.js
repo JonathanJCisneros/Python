970465f09dd6bc5192e869f2a742fd9304f7a7f8
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-06-21 09:35:50 -0700

    update

--- algorithms/week3/day2.js
+++ algorithms/week3/day2.js
@@ -9,22 +9,22 @@ Bonus (alumni interview):
     return how many times the given number occurs
 */
 
-const nums1 = [1, 3, 5, 6];
-const searchNum1 = 4;
-const expected1 = false;
+// const nums1 = [1, 3, 5, 6];
+// const searchNum1 = 4;
+// const expected1 = false;
 
-const nums2 = [4, 5, 6, 8, 12];
-const searchNum2 = 5;
-const expected2 = true;
+// const nums2 = [4, 5, 6, 8, 12];
+// const searchNum2 = 5;
+// const expected2 = true;
 
-const nums3 = [3, 4, 6, 8, 12];
-const searchNum3 = 3;
-const expected3 = true;
+// const nums3 = [3, 4, 6, 8, 12];
+// const searchNum3 = 3;
+// const expected3 = true;
 
-// bonus, how many times does the search num appear?
-const nums4 = [2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9];
-const searchNum4 = 2;
-const expected4 = 4;
+// // bonus, how many times does the search num appear?
+// const nums4 = [2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9];
+// const searchNum4 = 2;
+// const expected4 = 4;
 
 /**
  * Efficiently determines if the given num exists in the given array.
