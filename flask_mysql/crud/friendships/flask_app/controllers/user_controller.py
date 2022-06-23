43624ff4cbd134746b450a57611de8c4e50e093c
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-06-23 14:47:48 -0700

    friendships update

--- flask_mysql/crud/friendships/flask_app/controllers/user_controller.py
+++ flask_mysql/crud/friendships/flask_app/controllers/user_controller.py
@@ -5,5 +5,5 @@ from flask_app.models.user_model import User
 @app.route("/")
 @app.route("/friendships")
 def display_all():
-    user_list = User.get_all()
+    user_list = User.get_all_friendships()
     return render_template("friendships.html", user_list = user_list)
--- flask_mysql/crud/friendships/flask_app/models/user_model.py
+++ flask_mysql/crud/friendships/flask_app/models/user_model.py
@@ -1,6 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import DATABASE
-from flask_app.controllers import user_controller
 
 class User:
     def __init__(self, data):
@@ -9,14 +8,27 @@ class User:
         self.last_name = data['last_name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.friend = []
 
     @classmethod
-    def get_all(cls):
+    def get_all_friendships(cls):
         query =  "SELECT * "
         query += "FROM users "
         query += "LEFT JOIN friendships ON users.id = friendships.user_id "
         query += "LEFT JOIN users AS users2 ON friendships.friend_id = users2.id;"
 
         result = connectToMySQL(DATABASE).query_db(query)
-        print(result)
-        return result
+        
+        friends = cls(result[0])
+
+        for friend in result:
+            data = {
+                "friend_id" : friend['users2.id'],
+                "friend_first" : friend['users2.first_name'],
+                "friend_last" : friend['users2.last_name'],
+                "friend_created_at" : friend['users2.created_at'],
+                "friend_updated_at" : friend['users2.updated_at']
+            }
+            friends.friend.append(User(data))
+            return friends
+                
